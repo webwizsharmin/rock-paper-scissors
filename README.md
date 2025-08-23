36bef4933387423369a3db406a657c26ef49e922
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-08-23 06:50:50 +0600

    feat: implement basic rock paper scissors game with scoring system

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # rock-paper-scissors
 This is a console based game. 
-Basic Javascript
+Basic Javascript.
+
+
+## Algorithm / process
+1.computer choice(random)
+2.player choice
+3.compare both
+4.Decide the answer
+
+## functionality
+1.Random computer choice: I havae used math.random() and as we have to return 3 word like "rock", "paper" and "scissors" so for returning only 3 interger number mutiplyed by 3 (parameter). Then if else statement.
+
+2.User choice: prompt in a variable.
+
+3.compare: if else statement. compare the value between computer choice and the user choice.
+
+4.Finally the result.
--- app.js
+++ app.js
@@ -1,1 +1,70 @@
-console.log("Hello World!")
+// function for random result of rock papaer scissors
+
+function getComputerChoice(max) {
+    let choice = Math.floor(Math.random() * max);
+
+    switch (choice) {
+        case 0: return "rock";
+        case 1: return "paper";
+        default: return "scissors";
+    }
+}
+
+
+// For human input
+
+function getHumanChoice() {
+    let guess = prompt("Please enter your guess:");
+    return guess;
+}
+
+
+// comparing the result 
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computeChoice) {
+
+    if (humanChoice == computeChoice) {
+        return "Draw";
+    } 
+    
+    if (humanChoice == "rock" && computeChoice == "scissors") {
+        humanScore++;
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice == "paper" && computeChoice == "rock") {
+        humanScore++;
+        return "You win! Paper beats Rock";
+    } else if (humanChoice == "scissors" && computeChoice == "paper") {
+        humanScore++;
+        return "You win! Scissors beats Paper";
+    } else {
+        computerScore++;
+        return `You lose! ${computeChoice} beats ${humanChoice}`;
+    }
+
+}
+
+//5 times loop round
+
+for (let i = 0; i < 5; i++) 
+    {const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice(3);
+        
+        console.log(playRound(humanSelection, computerSelection));
+        console.log(`Scores -> You: ${humanScore} | Computer: ${computerScore}`);
+        console.log("---------------------------");
+    }
+
+    // the final resultd
+
+if (humanScore > computerScore) {
+    console.log("🎉 You won the game!");
+} else if (humanScore < computerScore) {
+    console.log("💻 Computer won the game!");
+} else {
+    console.log("🤝 The game is a draw!");
+}
+
+
